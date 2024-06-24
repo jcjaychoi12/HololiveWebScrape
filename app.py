4f182318d8b4da5311bdc2d314d9f9d70a12f703
jcjaychoi12 <jungbin.l.choi@gmail.com>
2024-06-24 11:33:13 -0400

    Implemented getInfo() to get social media links

--- app.py
+++ app.py
@@ -13,15 +13,27 @@ def main() -> None:
     talent_list = soup.find("ul", class_="talent_list clearfix")
     talent_list_item = talent_list.find_all("li")
 
-    talent_link = []
-    for t in talent_list_item:
-        talent_link.append(t.find_all("a")[0].get("href"))
-
+    talent_link = [link.find("a").get("href") for link in talent_list_item]
     print(talent_link)
 
+    print(getInfo(talent_link[0]))
 
-def getInfo(url: str) -> None:
-    pass
+
+def getInfo(url: str) -> dict:
+    result: dict = {}
+
+    r = requests.get(url)
+
+    soup = BeautifulSoup(r.content, "html.parser")
+    talent_article = soup.find("article", class_="in_talent single")
+
+    external_links_list = talent_article.find("ul", class_="t_sns clearfix")
+    external_links_list_item = [link.find("a").get("href") for link in external_links_list.find_all("li")]
+
+    result["Youtube"] = external_links_list_item[0].split('?', 1)[0]
+    result["Twitter/X"] = external_links_list_item[1]
+
+    return result
 
 
 if __name__ == "__main__":
