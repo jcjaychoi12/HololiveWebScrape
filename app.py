b68fe8d690a80b8e1739bc678e859090701a388d
jcjaychoi12 <jungbin.l.choi@gmail.com>
2024-06-24 11:49:18 -0400

    Implemented getInfo() to get talent's data

--- app.py
+++ app.py
@@ -7,14 +7,12 @@ HOLOLIVE_TALENT_MAIN: str = "https://hololive.hololivepro.com/en/talents"
 
 def main() -> None:
     r = requests.get(HOLOLIVE_TALENT_MAIN)
-    print(r)
 
     soup = BeautifulSoup(r.content, "html.parser")
     talent_list = soup.find("ul", class_="talent_list clearfix")
     talent_list_item = talent_list.find_all("li")
 
     talent_link = [link.find("a").get("href") for link in talent_list_item]
-    print(talent_link)
 
     print(getInfo(talent_link[0]))
 
@@ -32,6 +30,14 @@ def getInfo(url: str) -> dict:
 
     result["Youtube"] = external_links_list_item[0].split('?', 1)[0]
     result["Twitter/X"] = external_links_list_item[1]
+
+    talent_data_box = talent_article.find("div", class_="talent_data").find("div", class_="table_box")
+    talent_data_list = talent_data_box.find_all("dl")
+    
+    for dl in talent_data_list:
+        dt = dl.find("dt").text
+        dd = dl.find("dd").text
+        result[dt] = dd
 
     return result
 
